b0aeb0ce09c7ff4950b1975d4dced878cad7837a
Alan Kerby <alan.kerby@audacia.co.uk>
2025-06-18 14:54:34 +0100

    f1 score

--- tests/azure_ai_evaluation/test_azure_ai_similarity_evaluators.py
+++ tests/azure_ai_evaluation/test_azure_ai_similarity_evaluators.py
@@ -2,7 +2,7 @@ from typing import Optional
 import pytest
 from llm_eval.azure_ai_evaluation.azure_ai_similarity_evaluators import (
     RunSimilarityEvaluator,
-    AzureOpenAIModelConfiguration,
+    AzureOpenAIModelConfiguration, RunF1ScoreEvaluator,
 )
 
 
@@ -10,20 +10,20 @@ from llm_eval.azure_ai_evaluation.azure_ai_similarity_evaluators import (
     "query, response, ground_truth, threshold, model_config",
     [
         (
-            "Is Marie Curie is born in Paris?",
-            "According to wikipedia, Marie Curie was not born in Paris but in Warsaw.",
-            "Marie Curie was born in Warsaw.",
-            4.5,
-            None,
+                "Is Marie Curie is born in Paris?",
+                "According to wikipedia, Marie Curie was not born in Paris but in Warsaw.",
+                "Marie Curie was born in Warsaw.",
+                4.5,
+                None,
         )
     ],
 )
-def test_run_similarity_evaluator_passes(
-    query: str,
-    response: str,
-    ground_truth: str,
-    threshold: float,
-    model_config: Optional[AzureOpenAIModelConfiguration],
+def test_run_similarity_evaluator_should_pass(
+        query: str,
+        response: str,
+        ground_truth: str,
+        threshold: float,
+        model_config: Optional[AzureOpenAIModelConfiguration],
 ):
     RunSimilarityEvaluator(
         query=query,
@@ -32,3 +32,63 @@ def test_run_similarity_evaluator_passes(
         threshold=threshold,
         model_config=model_config,
     ).evaluate()
+
+
+@pytest.mark.parametrize(
+    "query, response, ground_truth, threshold, model_config",
+    [
+        (
+            "Is Marie Curie is born in Paris?",
+            "According to wikipedia, Marie Curie was not born in Paris but in Warsaw.",
+            "Marie Curie was born in London.",
+            3.0,
+            None,
+        )
+    ],
+)
+def test_run_similarity_evaluator_should_fail(
+        query: str,
+        response: str,
+        ground_truth: str,
+        threshold: float,
+        model_config: Optional[AzureOpenAIModelConfiguration],
+):
+    with pytest.raises(AssertionError):
+        RunSimilarityEvaluator(
+            query=query,
+            response=response,
+            ground_truth=ground_truth,
+            threshold=threshold,
+            model_config=model_config,
+        ).evaluate()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "response, ground_truth, threshold",
+    [
+        (
+            "According to wikipedia, Marie Curie was not born in Paris but in Warsaw.",
+            "Marie Curie was born in Warsaw.",
+            0.5
+        ),
+    ],
+)
+async def test_f1_score_evaluator_should_pass(response, ground_truth, threshold):
+    await RunF1ScoreEvaluator(response, ground_truth, threshold).evaluate()
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "response, ground_truth, threshold",
+    [
+        (
+            "According to wikipedia, Marie Curie was not born in Paris but in Warsaw.",
+            "Marie Curie was born in Warsaw.",
+            0.9
+        ),
+    ],
+)
+async def test_f1_score_evaluator_should_fail(response, ground_truth, threshold):
+    with pytest.raises(AssertionError):
+        await RunF1ScoreEvaluator(response, ground_truth, threshold).evaluate()
+
