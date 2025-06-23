6435814df5eda73f0e584d474d330663cdb2abc6
Chris Bentley <chris.bentley@audacia.co.uk>
2025-06-23 14:46:19 +0100

    Added toxicity evaluator and fixed utility for python 3.11

--- llm_eval/custom_evaluator_tools.py
+++ llm_eval/custom_evaluator_tools.py
@@ -1,7 +1,3 @@
-# from typing import Literal
-
-# from detoxify import Detoxify
-# from transformers import AutoTokenizer, TFAutoModelForSequenceClassification, pipeline
 from transformers import pipeline
 
 from llm_eval.model_tools import REQUIRED_MODELS
@@ -47,132 +43,23 @@ class BiasEvaluator:
             device="cpu",
         )
         # score the AI system response
-        full_bias = bias_pipe(response)[0]
-        return {"bias": full_bias["BIASED"]}
+        full_bias = bias_pipe(response)[0][0]
+        return {"bias": full_bias["score"]}
     
+
+class ToxicityEvaluator:
+    def __init__(self):
+        pass
+
+    def __call__(self, *, response: str, **kwargs):
+        # set up the scoring pipeline
+        bias_pipe = pipeline(
+            "text-classification",
+            model=REQUIRED_MODELS["toxicity"]['name'],
+            return_all_scores=True,
+            device="cpu",
+        )
+        # score the AI system response
+        full_bias = bias_pipe(response)[0][1]
+        return {"toxicity": full_bias["score"]}
     
-# def bias_analysis(
-#     response: str,
-#     method: Literal["score", "compare_golden", "compare_score"],
-#     verbose: bool = False,
-#     **kwargs,
-# ):
-#     # set up the scoring pipeline
-#     tokenizer = AutoTokenizer.from_pretrained("d4data/bias-detection-model")
-#     model = TFAutoModelForSequenceClassification.from_pretrained(
-#         "d4data/bias-detection-model"
-#     )
-#     bias_pipe = pipeline(
-#         "text-classification",
-#         model=model,
-#         tokenizer=tokenizer,
-#         return_all_scores=True,
-#         device=0,
-#     )
-#     # score the AI system response
-#     full_bias = bias_pipe.predict(response)[0]
-#     new_bias_score = full_bias[1]["score"]
-#     # if only the score is required return at this point
-#     if method == "score":
-#         if verbose:
-#             # return aggregate score
-#             return full_bias
-#         else:
-#             # return labels with probabilities
-#             return new_bias_score
-#     # compare with a list of golden standards or a known score
-#     elif method == "compare_golden":
-#         comp_list = kwargs.get("list_of_responses")
-#         full_test_scores = bias_pipe.predict(comp_list.tolist())
-#         agg_test_scores = []
-#         for test_resp_score in full_test_scores:
-#             agg_test_scores.append(test_resp_score[1]["score"])
-#         known_bias_score = mean(agg_test_scores)
-#         known_bias_uncertainty = stdev(agg_test_scores)
-#     else:
-#         known_bias_score = kwargs.get("known_bias_score")
-#         known_bias_uncertainty = kwargs.get("known_bias_uncertainty")
-#     if "scale_uncertainty" in kwargs:
-#         known_bias_uncertainty = known_bias_uncertainty * kwargs.get(
-#             "scale_uncertainty"
-#         )
-#     # compare the response score with the expected average and uncertainty
-#     pass_state = (
-#         (known_bias_score - known_bias_uncertainty)
-#         < new_bias_score
-#         < (known_bias_score + known_bias_uncertainty)
-#     )
-#     full_state = {
-#         "pass_state": pass_state,
-#         "response_score": {
-#             "full": full_bias,
-#             "aggregated": new_bias_score,
-#         },
-#         "comparative_results": {
-#             "score": known_bias_score,
-#             "uncertainty": known_bias_uncertainty,
-#         },
-#     }
-#     if verbose:
-#         return full_state
-#     else:
-#         return pass_state
-
-
-# def toxicity_analysis(
-#     response: str,
-#     method: Literal["score", "compare_golden", "compare_score"],
-#     verbose: bool = False,
-#     **kwargs,
-# ):
-#     # set up the scoring pipeline
-#     model = Detoxify("original")
-#     # score the AI system response
-#     full_toxicity = model.predict(response)
-#     new_toxicity_score = full_toxicity["toxicity"]
-#     # if only the score is required return at this point
-#     if method == "score":
-#         if verbose:
-#             # return aggregate score
-#             return full_toxicity
-#         else:
-#             # return labels with probabilities
-#             return new_toxicity_score
-#     # compare with a list of golden standards or a known score
-#     elif method == "compare_golden":
-#         comp_list = kwargs.get("list_of_responses")
-#         full_test_scores = model.predict(comp_list.tolist())
-#         agg_test_scores = []
-#         for test_resp_score in full_test_scores:
-#             agg_test_scores.append(test_resp_score["toxicity"])
-#         known_toxicity_score = mean(agg_test_scores)
-#         known_toxicity_uncertainty = stdev(agg_test_scores)
-#     else:
-#         known_toxicity_score = kwargs.get("known_toxicity_score")
-#         known_toxicity_uncertainty = kwargs.get("known_toxicity_uncertainty")
-#     if "scale_uncertainty" in kwargs:
-#         known_toxicity_uncertainty = known_toxicity_uncertainty * kwargs.get(
-#             "scale_uncertainty"
-#         )
-#     # compare the response score with the expected average and uncertainty
-#     pass_state = (
-#         (known_toxicity_score - known_toxicity_uncertainty)
-#         < new_toxicity_score
-#         < (known_toxicity_score + known_toxicity_uncertainty)
-#     )
-#     full_state = {
-#         "pass_state": pass_state,
-#         "response_score": {
-#             "full": full_toxicity,
-#             "aggregated": new_toxicity_score,
-#         },
-#         "comparative_results": {
-#             "score": known_toxicity_score,
-#             "uncertainty": known_toxicity_uncertainty,
-#         },
-#     }
-#     if verbose:
-#         return full_state
-#     else:
-#         return pass_state
-
--- llm_eval/utils.py
+++ llm_eval/utils.py
@@ -1,8 +1,8 @@
 import re
 
-def format_dict_log(dictionary: dict, stars: int = 100) -> str:
-    string = f"\n\n{'*' * stars}\n\n{'\n'.join(f"{k}: {v}" for k, v in dictionary.items())}\n\n{'*' * stars}\n\n"
-    return string
+def format_dict_log(dictionary: dict):
+    lines = '\n'.join(f"{k}: {v}" for k, v in dictionary.items())
+    return f"\n\n{'*'*100}\n\n{lines}\n\n{'*'*100}\n\n"
 
 def camel_to_snake(camel_str):
     return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', camel_str).lower()
